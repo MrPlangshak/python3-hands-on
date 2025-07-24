5ded316ae92260b90a3e9bd12548ee94ec48cc81
Mrplangshak <dankyesplang@gmail.com>
2025-07-24 16:26:33 +0000

    task2

---
+++ task_two.py
@@ -0,0 +1,28 @@
+"""
+Task 2: Film Night Prep
+You're helping your community plan a Friday Film Night for kids. The initial list of movie genres to be shown includes:
+genres = ["Adventure", "Comedy", "Animation", "Fantasy", "Sci-Fi", "Documentary", "Fantasy"]
+
+1. Add the genre "Drama" at the end of the list because parents requested it.
+2. Someone mistakenly added "Fantasy" twice. Make sure there's only one "Fantasy" in the list.
+3. The organizer wants to know how many genres are now planned to be shown.
+4. Display the second and second-to-last genres to verify diversity.
+
+→ Modify the list and display the required results.
+"""
+genres = ["Adventure", "Comedy", "Animation", "Fantasy", "Sci-Fi", "Documentary", "Fantasy"]
+
+#1. TO ADD DRAMA
+genres.append("Drama")
+print (genres)
+
+#2.to sort
+genres.sort()
+print(genres)
+
+#3.Howmany genre
+print(len(genres))
+
+#4. Display second and second to last
+print(genres[1])
+print(genres[6])
