58c73d1e023b64de1b522fc78ae276ee59b472fd
Mrplangshak <dankyesplang@gmail.com>
2025-07-24 16:36:35 +0000

    task3

---
+++ task_three.py
@@ -0,0 +1,24 @@
+"""
+Task 4: Pocket Money Records
+You're building a small tracker for your younger sibling's weekly pocket money.
+The amounts (in naira) for the past 5 weeks are stored like this:
+money = [1000, 1200, 800, 1500, 1100]
+
+1. Calculate and display the total amount received so far.
+2. A mistake was made in the third week's entry (800). It should have been 1000. Fix it.
+3. Display the list in reverse order to check most recent payments first.
+
+→ Perform the corrections and computations, and print all results.
+"""
+
+money = [1000, 1200, 800, 1500, 1100]
+
+#1
+print(money[0] + money[1] + money[2] + money[3] + money[4])
+
+#2
+money[2] = 1000
+print(money)
+
+#3
+print(money[::-1])
